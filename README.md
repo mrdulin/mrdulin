ef7b3b3cdc5c9dfe3d09ea998ca04d42ac51e0c4
mrdulin <novaline@qq.com>
2017-04-07 17:44:35 +0800

    add some examples

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # css training
 
+** HOW TO USE **
+
+* `npm i`
+* `./node_modules/.bin/gulp webserver`
+
 __TODO:__
 
 - [] `position:sticky`属性，IOS下`input` `focus`后，`sticky`是否能将`header`置顶
---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+
+gulp.task('webserver', function() {
+  gulp.src('examples')
+    .pipe(webserver({
+      host: '0.0.0.0',
+      livereload: true,
+      open: true
+    }));
+});
+
+
